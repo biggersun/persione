1e1b0766074e9d615e45b60d5ca80c8133c21a77
Sun <2659528347@qq.com>
2015-10-15 00:02:20 +0800

    2015-10-15

--- js/index.js
+++ js/index.js
@@ -4,62 +4,61 @@
                 if(!opt){
                     var opt={};
                 }
-                var _btnLeft = $("#"+ opt.left);
-                var _btnRight = $("#"+ opt.right);
+                var _btnDown = $("#"+ opt.left);
+                var _btnUp = $("#"+ opt.right);
                 var timerID;
                 var _this=this.find("ul:first");
-                var ImgWid=_this.find("li:first").width();
+                var imgHeight = _this.find("li:first").height();
+                //var ImgWid=_this.find("li:first").width();
                 var speed=opt.speed?parseInt(opt.speed,10):500;
                 var timer=opt.timer;
-                var leftWid=0-ImgWid;
-                var _index = this.index();
+                var upHeight = 0-imgHeight;
+                //var leftWid=0-ImgWid;
+                //var _index = this.index();
                  
                 //滚动函数
-                var scrollLeft=function(){
-                        _btnLeft.unbind("click",scrollRight); //Shawphy:取消向左按钮的函数绑定
+                var scrollDown=function(){
+                        _btnDown.unbind("click",scrollUp); //Shawphy:取消向左按钮的函数绑定
                         _this.animate({
-                                marginLeft: leftWid
+                                marginTop: upHeight
                         },speed,function(){
-                                    // for(var i=0;i<indexAll;i++){
-                                    //     _this.find("li:eq("+i+")").appendTo(_this);                                
-                                    // }
-                                if (_index==0) {};
-                                _this.find("li:eq("+_index-1+")").appendTo(_this);
-                                _this.css({marginLeft:0});
-                                _btnLeft.bind("click",scrollRight); //Shawphy:绑定向左按钮的点击事件
+                                    for(i=1;i<=line;i++){
+                                        _this.find("li:first").appendTo(_this);
+                                    }
+                                //if (_index==0) {};
+                                //_this.find("li:eq("+_index-1+")").appendTo(_this);
+                                _this.css({marginTop:0});
+                                _btnDown.bind("click",scrollUp); //Shawphy:绑定向左按钮的点击事件
                         });
-                        console.log(_index);
+                       // console.log(_index);
 
                 };
                 //Shawphy:向右翻页函数
-                var scrollRight=function(){
-                        _btnRight.unbind("click",scrollLeft);
-                            // for(var i=0;i<indexAll;i++){
-                            //        _this.find("li:eq("+i+")").show().prependTo(_this);                               
-                            //     }
-                       // this.j=0;
-                        // for(var i=0;i<indexAll;i++){j++;}
-                       // _this.find("li:eq("+_index+")").show().prependTo(_this);
-                        _this.find("li:eq("+_index+1+")").appendTo(_this);
-                        _this.css({marginLeft:leftWid});
+                var scrollUp=function(){
+                        _btnUp.unbind("click",scrollDown);
+                            for(i=1;i<=line;i++){
+                                _this.find("li:last").show().prependTo(_this);
+                            }
+                       // _this.find("li:eq("+_index+1+")").prependTo(_this);
+                        _this.css({marginTop:upHeight});
                         
                         _this.animate({
-                                marginLeft:0
+                                marginTop:0
                         },speed,function(){
-                                _btnRight.bind("click",scrollLeft);
+                                _btnUp.bind("click",scrollDown);
                         });
                 };
                //Shawphy:自动播放
                 var autoPlay = function(){
-                        if(timer)timerID = window.setInterval(scrollRight,timer);
+                        if(timer)timerID = window.setInterval(scrollUp,timer);
                 };
                 var autoStop = function(){
                         if(timer)window.clearInterval(timerID);
                 };
                  //鼠标事件绑定
                 _this.hover(autoStop,autoPlay).mouseout();
-                _btnLeft.css("cursor","pointer").click( scrollLeft ).hover(autoStop,autoPlay);//Shawphy:向左向右鼠标事件绑定
-                _btnRight.css("cursor","pointer").click( scrollRight ).hover(autoStop,autoPlay);
+                _btnDown.css("cursor","pointer").click( scrollDown ).hover(autoStop,autoPlay);//Shawphy:向左向右鼠标事件绑定
+                _btnUp.css("cursor","pointer").click( scrollUp ).hover(autoStop,autoPlay);
 
 			}
 
