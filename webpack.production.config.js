95fa1f35b976d33f7c947da3522d306e3797e33d
biggersun <2659528347@qq.com>
2017-02-18 23:52:51 +0800

    add production.config

---
+++ webpack.production.config.js
@@ -0,0 +1,22 @@
+const webpack = require('webpack');
+const path = require('path');
+const node_modules_dir = path.resolve(__dirname, 'node_modules');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  module: {
+    loaders: [{
+      test: /\.js[x]?$/,
+      exclude: [node_modules_dir],
+      loader: 'babel-loader',
+      query: {
+        presets: ['es2015', 'react'],
+      },
+      include: path.join(__dirname, '.'),
+    }]
+  },
+}
